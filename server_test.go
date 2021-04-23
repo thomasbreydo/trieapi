b625edcc29047fec2168a3bd7055a317b96ed1fd
Thomas Breydo <tbreydo@gmail.com>
2021-04-23 10:09:25 -0400

    Strengthen tests

--- server_test.go
+++ server_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/thomasbreydo/trieapi/cli/trie/cmd"
@@ -81,4 +82,82 @@ func TestServer(t *testing.T) {
 		t.Error(err)
 	}
 	check("Keyword (test) missing", buf, t)
+
+	// 0
+	cmd.Reset()
+	cmd.Root.SetArgs([]string{"add", "--word", ""})
+	if err := cmd.Root.Execute(); err != nil {
+		t.Error(err)
+	}
+	check("Keyword () added", buf, t)
+	// 1
+	cmd.Reset()
+	cmd.Root.SetArgs([]string{"add", "--word", "h"})
+	if err := cmd.Root.Execute(); err != nil {
+		t.Error(err)
+	}
+	check("Keyword (h) added", buf, t)
+	// 2
+	cmd.Reset()
+	cmd.Root.SetArgs([]string{"add", "--word", ""})
+	if err := cmd.Root.Execute(); err != nil {
+		t.Error(err)
+	}
+	check("Keyword () present", buf, t)
+	// 3
+	cmd.Reset()
+	cmd.Root.SetArgs([]string{"delete", "--word", "test"})
+	if err := cmd.Root.Execute(); err != nil {
+		t.Error(err)
+	}
+	check("Keyword (test) missing", buf, t)
+	// 4
+	cmd.Reset()
+	cmd.Root.SetArgs([]string{"complete", "-p", ""})
+	if err := cmd.Root.Execute(); err != nil {
+		t.Error(err)
+	}
+	check("\nh", buf, t)
+	// 5
+	cmd.Reset()
+	cmd.Root.SetArgs([]string{"search", "--word", "test"})
+	if err := cmd.Root.Execute(); err != nil {
+		t.Error(err)
+	}
+	check("Keyword (test) not found", buf, t)
+	// 6
+	cmd.Reset()
+	cmd.Root.SetArgs([]string{"search", "--word", ""})
+	if err := cmd.Root.Execute(); err != nil {
+		t.Error(err)
+	}
+	check("Keyword () found", buf, t)
+	// 7
+	cmd.Reset()
+	cmd.Root.SetArgs([]string{"add", "--word", "hello"})
+	if err := cmd.Root.Execute(); err != nil {
+		t.Error(err)
+	}
+	check("Keyword (hello) added", buf, t)
+	// 8
+	cmd.Reset()
+	cmd.Root.SetArgs([]string{"add", "--word", "helli"})
+	if err := cmd.Root.Execute(); err != nil {
+		t.Error(err)
+	}
+	check("Keyword (helli) added", buf, t)
+	// 9
+	cmd.Reset()
+	cmd.Root.SetArgs([]string{"complete", "--prefix", "hel"})
+	if err := cmd.Root.Execute(); err != nil {
+		t.Error(err)
+	}
+	b, err := ioutil.ReadAll(buf)
+	if err != nil {
+		t.Error(err)
+	}
+	out := strings.Split(string(b), "\n")
+	if out[0] == "hello" && out[1] != "helli" || out[0] == "helli" && out[1] != "hello" {
+		t.Errorf("desired: hello\nhelli\nactual:%s", out)
+	}
 }
